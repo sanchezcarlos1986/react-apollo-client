aa34e5825ce171d420212362e674f6819f31fc15
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2019-05-18 18:28:16 -0400

    feat(router): getting queries using variables

--- src/App.js
+++ src/App.js
@@ -1,51 +1,26 @@
-import React from 'react';
+import React from 'react'
 import ApolloClient from 'apollo-boost'
-import { ApolloProvider, Query } from 'react-apollo'
-import gql from 'graphql-tag'
-import logo from './logo.svg';
-import './App.css';
+import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Posts from './Posts/Posts'
+import Post from './Posts/Post'
 
 const client = new ApolloClient({
   uri: 'https://api-useast.graphcms.com/v1/cjqtzxlhd2j3g01dnwortupa6/master'
 })
 
-const POSTS_QUERY = gql`
-  query allPosts {
-    posts {
-      id
-      title
-      body
-    }
-  }
-`
-
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Query query={POSTS_QUERY}>
-            {({ loading, error, data }) => {
-              if (loading) return <p>Loading...</p>
-              if (error) return <p>Error...</p>
-              const { posts } = data
-              return posts.map(({ id, title }) => <h1 key={id}>{title}</h1>)
-            }}
-          </Query>
-        </header>        
+        <Switch>
+          <Route exact path='/' component={Posts} />
+          <Route exact path='/post/:id' component={Post} />
+        </Switch> 
       </div>
+      </Router>
     </ApolloProvider>
   );
 }
