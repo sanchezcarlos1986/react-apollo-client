9f9f391bb468dc5db985e3627c86bb4131ed3218
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2019-05-19 01:29:13 -0400

    feat(mutations): saving data from a react form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 import Posts from './Posts/Posts'
 import Post from './Posts/Post'
+import NewPost from './Posts/NewPost'
 
 const client = new ApolloClient({
   uri: 'https://api-useast.graphcms.com/v1/cjqtzxlhd2j3g01dnwortupa6/master'
@@ -15,9 +16,15 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
       <div className="App">
+        <header>
+          <Link to="/">Home</Link>
+          <Link to="/posts/new">Add Post</Link>
+          <hr />
+        </header>
         <Switch>
           <Route exact path='/' component={Posts} />
           <Route exact path='/post/:id' component={Post} />
+          <Route exact path='/posts/new' component={NewPost} />
         </Switch> 
       </div>
       </Router>
