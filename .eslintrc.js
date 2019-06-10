87b53bfe04ac1076a4cb339b4cbda7ec631908ba
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2019-06-09 21:34:56 -0400

    chore(eslint): added eesling

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "env": {
+      "browser": true,
+      "es6": true
+  },
+  "settings": {
+        "ecmascript": 6,
+        "jsx": true
+  },
+  "parserOptions": {
+      "ecmaVersion": 2017,
+      "ecmaFeatures": {
+          "experimentalObjectRestSpread": true,
+          "experimentalDecorators": true,
+          "jsx": true
+      },
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react",
+  ],
+  "extends": "airbnb",
+  "rules": {
+    "react/jsx-filename-extension": 0,
+    "function-paren-newline": 0,
+    "semi": 0,
+    "comma-dangle": 0,
+    "react-hooks/exhaustive-deps": "none"
+  },
+  "globals": {
+    "test": true,
+    "expect": true,
+    "afterEach": true,
+    "jest": true,
+    "describe": true,
+    "beforeAll": true,
+    "afterAll": true,
+  }
+};
